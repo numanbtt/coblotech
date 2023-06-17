d2cf0dc2d501df6b3e5a4cc6c194cb4983876064
Numan <numanbtt@gmail.com>
2023-06-17 11:34:42 +0500

    chore: Code Cleaning

--- server/src/routes/getPost.ts
+++ server/src/routes/getPost.ts
@@ -5,12 +5,12 @@ import { Client, Discussion } from '@hiveio/dhive'
 const route = express.Router()
 //connect to server which is connected to the network/production
 const client = new Client('https://api.hive.blog');
-let opts: any = {};
+// let opts: any = {};
 
-//connect to production server
-opts.addressPrefix = 'STM';
-opts.chainId =
-    'beeab0de00000000000000000000000000000000000000000000000000000000';
+// //connect to production server
+// opts.addressPrefix = 'STM';
+// opts.chainId =
+//     'beeab0de00000000000000000000000000000000000000000000000000000000';
 
 route.get('/getPost', async (req, res) => {
     const filter = req.body.filter;
@@ -20,12 +20,30 @@ route.get('/getPost', async (req, res) => {
     };
     let result = await client.database.getDiscussions(filter, query)
     try {
-        console.log("author: ", result[0].author);
-        console.log("title: ", result[0].title);
-        res.send(result[0])
+        result.forEach((post) => {
+            console.log("author: ", post.author);
+            console.log("title: ", post.title);
+        })
+        res.send(result)
     } catch (error) {
         console.log("error: ", error);
     }
 })
 
+route.get('/getBlog', async (req, res) => {
+    const query = {
+        tag: req.body.username,
+        limit: req.body.limit,
+    };
+    let result = await client.database.getDiscussions('blog', query)
+    try {
+        result.forEach((post) => {
+            console.log("author: ", post.author);
+            console.log("title: ", post.title);
+        })
+        res.send(result)
+    } catch (error) {
+        console.log("error: ", error);
+    }
+})
 export default route
